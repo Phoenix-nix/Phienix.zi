fb0ebc025b42e57eaaef6d9a67f5eb5c4ab86409
Phoenix.nix <phoenix.n@foxmail.com>
2020-04-02 23:31:18 +0800

    递归实现求n的阶乘

---
+++ text_print/text_print/text.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+int Fac(int n)
+{
+	if (n <= 1)
+		return 1;
+	else
+		return n*Fac(n - 1);
+}
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+
+
+	int ret = Fac(n);
+	printf("%d", ret);
+	return 0;
+}
