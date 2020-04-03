8a74b64d36b7a5ba25103e6c3961d2ac3754ff59
Phoenix.nix <phoenix.n@foxmail.com>
2020-04-03 22:31:04 +0800

    打印菱形

---
+++ text_diamond/text_diamond/text.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//打印菱形*
+int main()
+{
+	int line = 0;
+	scanf("%d", &line);//7
+	//打印上部分
+	int i = 0;
+	for (i = 0; i < line; i++)
+	{
+		//打印一行
+		//先打印空格
+		int j = 0;
+		for (j = 0; j < line - 1 - i; j++)
+		{
+			printf(" ");
+		}
+		//打印*
+		for (j = 0; j < 2 * i + 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	//打印下部分
+	for (i = 0; i < line - 1; i++)
+	{
+		//打印一行
+		//先打印空格
+		int j = 0;
+		for (j = 0; j < i + 1; j++)
+		{
+			printf(" ");
+		}
+		//
+		for (j = 0; j < 2 * (line - 1 - i) - 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
