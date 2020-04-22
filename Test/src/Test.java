c54f12eef05348b9171a5ae13571c439ee9eff25
Phoenix.nix <phoenix.n@foxmail.com>
2020-04-22 23:42:30 +0800

    写一个方法将数组中的每个元素都*2

---
+++ Test/src/Test.java
@@ -0,0 +1,20 @@
+public class Test {
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void transform(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] * 2;
+        }
+    }
+    public static void main(String[] args) {
+            int[] arr = {1, 2, 3};
+            transform(arr);
+            printArray(arr);
+    }
+
+
+}
