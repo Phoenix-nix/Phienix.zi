35ddcfe5c823829e76a0d6f75a8ec03f4800d131
Phoenix.nix <phoenix.n@foxmail.com>
2020-05-27 22:22:52 +0800

    若链表有环，返回链表开始入环的第一个节点。 若无环，则返回 null。

---
+++ getIntersectionNode/src/MyLinkedList.java
@@ -0,0 +1,58 @@
+class Node {
+    public int data;//数据
+    public Node next;//后继信息
+    public Node prev;//前驱信息
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
+public class MyLinkedList {
+    public Node head;//双向链表标志的头
+    public Node tail;//双向链表的尾巴
+
+    //头插法
+    public void addFirst(int data) {
+        Node node = new Node(data);
+        if (this.head == null) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            node.next = this.head;
+            this.head.prev = node;
+            this.head = node;
+        }
+    }
+
+    //打印双链表
+    public void display() {
+        Node cur = this.head;
+        while (cur != null) {
+            System.out.print(cur.data + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+
+    //给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+    public Node detectCycle() {
+        Node fast = this.head;
+        Node slow = this.head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) {
+                break;
+            }
+        }
+        if(fast == null || fast.next == null) {
+            return null;
+        }
+        slow = this.head;
+        while(fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
---
+++ getIntersectionNode/src/TestDemo.java
@@ -0,0 +1,16 @@
+public class TestDemo {
+
+    public static void main(String[] args) {
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addFirst(11);
+        myLinkedList.addFirst(12);
+        myLinkedList.addFirst(13);
+        myLinkedList.addFirst(14);
+        myLinkedList.addFirst(15);
+        myLinkedList.display();
+
+        System.out.println(myLinkedList.detectCycle());
+
+    }
+}
