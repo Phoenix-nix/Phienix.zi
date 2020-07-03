ce10d4a55e8da0bb6b5c204c0ef654495d1ce51e
Phoenix.nix <phoenix.n@foxmail.com>
2020-07-03 22:44:45 +0800

    解救小易

---
+++ save/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()) {
+            int n = in.nextInt();
+            int[] x = new int[n];
+            int[] y = new int[n];
+            for(int i = 0; i < n; i++) {
+                x[i] = in.nextInt();
+            }
+            for(int i = 0; i < n; i++) {
+                y[i] = in.nextInt();
+            }
+            int min = Integer.MAX_VALUE;
+            for(int i = 0; i < n; i++) {
+                if(x[i]+y[i]-2<min){
+                    min = x[i]+y[i]-2;
+                }
+            }
+            System.out.println(min);
+        }
+    }
+}
