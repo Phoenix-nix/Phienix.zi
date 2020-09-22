4aec6cfc6fee35244f0c466462617067487b9cce
Phoenix.nix <phoenix.n@foxmail.com>
2020-09-22 15:08:15 +0800

    项目

---
+++ A且听书吟/src/main/java/com/phoenix/service/BookService.java
@@ -0,0 +1,42 @@
+package com.phoenix.service;
+
+import com.phoenix.dao.BookDao;
+import com.phoenix.dao.SectionDao;
+import com.phoenix.model.Book;
+import com.phoenix.model.Section;
+import com.phoenix.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class BookService {
+    private BookDao bookDao;
+    private SectionDao sectionDao;
+
+    public BookService() {
+        bookDao = new BookDao();
+        sectionDao = new SectionDao();
+    }
+
+    public List<Book> list() throws SQLException {
+        return bookDao.selectAll();
+    }
+
+    public Book post(String title, User user) throws SQLException {
+        return bookDao.insert(user, title);
+    }
+
+    public Book get(int bid) throws SQLException {
+        Book book = bookDao.selectByBid(bid);
+        if (book == null) {
+            return null;
+        }
+        book.sections = sectionDao.selectByBid(bid);
+
+        return book;
+    }
+
+    public void addSection(int bid, String name) throws SQLException {
+        sectionDao.insert(bid, name);
+    }
+}
